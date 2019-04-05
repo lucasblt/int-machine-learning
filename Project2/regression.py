05e00da216a819a4242bf664a5a2f2c0d4e1a155
Everton Spader <Everton@evertons-mbp.eduroam.wireless.dtu.dk>
2019-04-05 18:39:18 +0200

    Nois que voa

--- Project2/regression.py
+++ Project2/regression.py
@@ -16,10 +16,7 @@ raw_data = df.get_values()
 
 X = raw_data[:,1:]
 N,M = X.shape
-y = np.zeros(shape=(N,1))
-
-classNames = ['CHD Negative','CHD Positive']
-C = len(classNames)
+y = np.zeros((5,1))
 
 cols = range(1, M+1)
 
@@ -33,132 +30,108 @@ famHistDict = dict(zip(famHistLabels,[0,1]))
 famHistLc = np.array([famHistDict[cl] for cl in famHistNames])
 X = np.hstack((X[:,0:4],np.array([famHistLc]).T,X[:,5:])).astype(np.float)
 
-## REGRESSION - PART A
+#Binary attributes to be removed from dataset
+famhist_idx = attributeNames.index('famhist')
+chd_idx = attributeNames.index('chd')
 
 #Attribute to be predicted based on the other attributes
 y_idx = attributeNames.index('ldl')
 y = X[:,y_idx]
 
-#Attributes to remove for regression
-famhist_idx = attributeNames.index('famhist')
-chd_idx = attributeNames.index('chd')
-
-#X_cols = list(range(1,famhist_idx))+list(range(famhist_idx+1,chd_idx))
 X_cols = list(range(0,y_idx))+ list(range(y_idx+1,famhist_idx))+list(range(famhist_idx+1,chd_idx))
-
 X = X[:,X_cols]
-
-#Removing y from atrributeNames
+N, M = X.shape
+#Remove y and binary attributes from atrributeNames
 attributeNames = attributeNames[0:y_idx] + attributeNames[y_idx+1:famhist_idx] + attributeNames[famhist_idx+1:chd_idx]
 
-########################## REGULARIZATION PARAMETER ##########################
-
-N, M = X.shape
-
+########################## REGRESSION - PART A ##########################
+# 1) REGULARIZATION PARAMETER
 # Add offset attribute
 X = np.concatenate((np.ones((X.shape[0],1)),X),1)
 attributeNames = [u'Offset']+attributeNames
 M = M+1
 
+
 ## Crossvalidation
 # Create crossvalidation partition for evaluation
-K = 5
-CV = model_selection.KFold(K, shuffle=True)
+K = 10
 
 # Values of lambda
 lambdas = np.power(10.,range(-2,9))
 
 # Initialize variables
-#T = len(lambdas)
-Error_train = np.empty((K,1))
-Error_test = np.empty((K,1))
-Error_train_rlr = np.empty((K,1))
-Error_test_rlr = np.empty((K,1))
-Error_train_nofeatures = np.empty((K,1))
-Error_test_nofeatures = np.empty((K,1))
 w_rlr = np.empty((M,K))
 mu = np.empty((K, M-1))
 sigma = np.empty((K, M-1))
 w_noreg = np.empty((M,K))
 
-k=0
+
+CV = model_selection.KFold(K, shuffle=True)
+w = np.empty((M,K,len(lambdas)))
+train_error = np.empty((K,len(lambdas)))
+test_error = np.empty((K,len(lambdas)))
+k = 0
+y = y.squeeze()
 
 for train_index, test_index in CV.split(X,y):
-    
-    # extract training and test set for current CV fold
     X_train = X[train_index]
     y_train = y[train_index]
     X_test = X[test_index]
     y_test = y[test_index]
-    internal_cross_validation = 5    
+        
+    # Standardize the training and set set based on training set moments
+    mu = np.mean(X_train[:, 1:], 0)
+    sigma = np.std(X_train[:, 1:], 0)
     
-    opt_val_err, opt_lambda, mean_w_vs_lambda, train_err_vs_lambda, test_err_vs_lambda = rlr_validate(X_train, y_train, lambdas, internal_cross_validation)
-
-    # Standardize outer fold based on training set, and save the mean and standard
-    # deviations since they're part of the model (they would be needed for
-    # making new predictions) - for brevity we won't always store these in the scripts
-    mu[k, :] = np.mean(X_train[:, 1:], 0)
-    sigma[k, :] = np.std(X_train[:, 1:], 0)
+    X_train[:, 1:] = (X_train[:, 1:] - mu) / sigma
+    X_test[:, 1:] = (X_test[:, 1:] - mu) / sigma
     
-    X_train[:, 1:] = (X_train[:, 1:] - mu[k, :] ) / sigma[k, :] 
-    X_test[:, 1:] = (X_test[:, 1:] - mu[k, :] ) / sigma[k, :] 
-    
+    # precompute terms
     Xty = X_train.T @ y_train
     XtX = X_train.T @ X_train
+    for l in range(0,len(lambdas)):
+        # Compute parameters for current value of lambda and current CV fold
+        lambdaI = lambdas[l] * np.eye(M)
+        lambdaI[0,0] = 0 # remove bias regularization
+        w[:,k,l] = np.linalg.solve(XtX+lambdaI,Xty).squeeze()
+        # Evaluate training and test performance
+        train_error[k,l] = np.power(y_train-X_train @ w[:,k,l].T,2).mean(axis=0)
+        test_error[k,l] = np.power(y_test-X_test @ w[:,k,l].T,2).mean(axis=0)
+
+    k=k+1
     
-    # Compute mean squared error without using the input data at all
-    Error_train_nofeatures[k] = np.square(y_train-y_train.mean()).sum(axis=0)/y_train.shape[0]
-    Error_test_nofeatures[k] = np.square(y_test-y_test.mean()).sum(axis=0)/y_test.shape[0]
+opt_val_err = np.min(np.mean(test_error,axis=0))
+opt_lambda = lambdas[np.argmin(np.mean(test_error,axis=0))]
+train_err_vs_lambda = np.mean(train_error,axis=0)
+test_err_vs_lambda = np.mean(test_error,axis=0)
+mean_w_vs_lambda = np.squeeze(np.mean(w,axis=1))
 
-    # Estimate weights for the optimal value of lambda, on entire training set
-    lambdaI = opt_lambda * np.eye(M)
-    lambdaI[0,0] = 0 # Do no regularize the bias term
-    w_rlr[:,k] = np.linalg.solve(XtX+lambdaI,Xty).squeeze()
-    # Compute mean squared error with regularization with optimal lambda
-    Error_train_rlr[k] = np.square(y_train-X_train @ w_rlr[:,k]).sum(axis=0)/y_train.shape[0]
-    Error_test_rlr[k] = np.square(y_test-X_test @ w_rlr[:,k]).sum(axis=0)/y_test.shape[0]
 
-    # Estimate weights for unregularized linear regression, on entire training set
-    w_noreg[:,k] = np.linalg.solve(XtX,Xty).squeeze()
-    # Compute mean squared error without regularization
-    Error_train[k] = np.square(y_train-X_train @ w_noreg[:,k]).sum(axis=0)/y_train.shape[0]
-    Error_test[k] = np.square(y_test-X_test @ w_noreg[:,k]).sum(axis=0)/y_test.shape[0]
+figure(figsize=(12,8))
 
-    # Display the results for the last cross-validation fold
-    if k == K-1:
-        figure(k, figsize=(12,8))
-        subplot(1,2,1)
-        semilogx(lambdas,mean_w_vs_lambda.T[:,1:],'.-') # Don't plot the bias term
-        xlabel('Regularization factor')
-        ylabel('Mean Coefficient Values')
-        grid()
+subplot(1,2,1)
+semilogx(lambdas,mean_w_vs_lambda.T[:,1:],'.-') # Don't plot the bias term
+xlabel('Regularization factor')
+ylabel('Mean Coefficient Values')
+grid()
         # You can choose to display the legend, but it's omitted for a cleaner 
         # plot, since there are many attributes
 #        legend(attributeNames[1:], loc='best')
         
-        subplot(1,2,2)
-        title('Optimal lambda: 1e{0}'.format(np.log10(opt_lambda)))
-        loglog(lambdas,train_err_vs_lambda.T,'b.-',lambdas,test_err_vs_lambda.T,'r.-')
-        xlabel('Regularization factor')
-        ylabel('Squared error (crossvalidation)')
-        legend(['Train error','Validation error'])
-        grid()
-
-    k+=1
-
+subplot(1,2,2)
+title('Optimal lambda: 1e{0}'.format(np.log10(opt_lambda)))
+loglog(lambdas,train_err_vs_lambda.T,'b.-',lambdas,test_err_vs_lambda.T,'r.-')
+xlabel('Regularization factor')
+ylabel('Squared error (crossvalidation)')
+legend(['Train error','Validation error'])
+grid()
 show()
-# Display results
-print('Linear regression without feature selection:')
-print('- Training error: {0}'.format(Error_train.mean()))
-print('- Test error:     {0}'.format(Error_test.mean()))
-print('- R^2 train:     {0}'.format((Error_train_nofeatures.sum()-Error_train.sum())/Error_train_nofeatures.sum()))
-print('- R^2 test:     {0}\n'.format((Error_test_nofeatures.sum()-Error_test.sum())/Error_test_nofeatures.sum()))
-print('Regularized linear regression:')
-print('- Training error: {0}'.format(Error_train_rlr.mean()))
-print('- Test error:     {0}'.format(Error_test_rlr.mean()))
-print('- R^2 train:     {0}'.format((Error_train_nofeatures.sum()-Error_train_rlr.sum())/Error_train_nofeatures.sum()))
-print('- R^2 test:     {0}\n'.format((Error_test_nofeatures.sum()-Error_test_rlr.sum())/Error_test_nofeatures.sum()))
 
+for i in range(len(lambdas)):
+    if lambdas[i] == opt_lambda:
+        opt_lambda_idx = i
+        
+# Display results
 print('Weights in last fold:')
 for m in range(M):
-    print('{:>15} {:>15}'.format(attributeNames[m], np.round(w_rlr[m,-1],2)))
+    print('{:>15} {:>15}'.format(attributeNames[m], np.round(w[m,-1,opt_lambda_idx],2)))
